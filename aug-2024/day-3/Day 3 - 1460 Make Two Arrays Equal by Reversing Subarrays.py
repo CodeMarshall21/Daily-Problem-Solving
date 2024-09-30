35445d7c730247faa8dad4373dd35528016663f3
keshav <k21keshav05@gmai.com>
2024-09-30 23:37:16 +0530

    deletedAUG

--- aug-2024/day-3/Day 3 - 1460 Make Two Arrays Equal by Reversing Subarrays.py
+++
@@ -1,9 +0,0 @@
-#approach-1
-class Solution:
-    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
-        return sorted(target) == sorted(arr)
-    
-#approach-2    
-class Solution:
-    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
-        return Counter(target) == Counter(arr)
