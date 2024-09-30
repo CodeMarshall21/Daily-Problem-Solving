8914d5caa492ff118d198bd187cb4ed52a1bb453
keshav <k21keshav05@gmai.com>
2024-09-30 23:57:18 +0530

    gfg

--- Position of rightmost different bit/Position of rightmost different bit-gfg.py
+++ Position of rightmost different bit/Position of rightmost different bit-gfg.py
@@ -1,4 +1,6 @@
 #Given two numbers m and n. Find the position of the rightmost different bit in the binary representation of numbers. It is guaranteed that such a bit exists
+
+
 import math
 num1 = int(input("Number 1 : "))
 num2 = int(input("Number 2 : "))
