77f2a235f54b4edbae2d9650b1a6686c82e06726
keshav <k21keshav05@gmail.com>
2025-03-28 00:02:12 +0530

    Minimum Index of a Valid Split

--- Minimum Index of a Valid Split/2780. Minimum Index of a Valid Split.py
+++ Minimum Index of a Valid Split/2780. Minimum Index of a Valid Split.py
@@ -3,11 +3,19 @@ class Solution:
         majorityElement = 0
         count = 0
 
+        # for n in nums:
+        #     if count == 0:
+        #         majorityElement = n
+        #         count = 1
+        #     elif n == majorityElement:
+        #         count += 1
+        #     else:
+        #         count -= 1
+        
         for n in nums:
             if count == 0:
                 majorityElement = n
-                count = 1
-            elif n == majorityElement:
+            if n == majorityElement:
                 count += 1
             else:
                 count -= 1
@@ -23,7 +31,7 @@ class Solution:
                 
                 leftLength = i + 1
                 rightLength = len(nums) - leftLength
-                
+
                 if leftCount > (leftLength//2) and rightCount >  (rightLength//2) :
                     return i
         return -1
