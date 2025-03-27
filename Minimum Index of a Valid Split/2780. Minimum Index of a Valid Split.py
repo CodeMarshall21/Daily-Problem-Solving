25465c8980dc2fd8ea66519c7b6f5279767bdd73
keshav <k21keshav05@gmail.com>
2025-03-27 23:57:36 +0530

    Minimum Index of a Valid Split

---
+++ Minimum Index of a Valid Split/2780. Minimum Index of a Valid Split.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+        majorityElement = 0
+        count = 0
+
+        for n in nums:
+            if count == 0:
+                majorityElement = n
+                count = 1
+            elif n == majorityElement:
+                count += 1
+            else:
+                count -= 1
+
+
+        leftCount = 0
+        rightCount = nums.count(majorityElement)
+
+        for i in range(len(nums)):
+            if nums[i] == majorityElement:
+                leftCount += 1
+                rightCount -= 1
+                
+            leftLength = i + 1
+            rightLength = len(nums) - leftLength
+            
+            if leftCount > (leftLength//2) and rightCount >  (rightLength//2) :
+                return i
+        return -1
